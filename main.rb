d01d67fb5c44188b000f3132ac4147944bea088a
eric <hyongzhe@mails.ccsf.edu>
2017-02-10 17:49:54 -0800

    fxxk

--- main.rb
+++ main.rb
@@ -9,9 +9,6 @@ configure :development do
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 end
 
-configure :production do
-  DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_RED_URL'])
-end
 
 
 configure do
