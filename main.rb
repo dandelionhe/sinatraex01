e7507b1945dfa45eb341cf9f755d2dae0addc431
eric <hyongzhe@mails.ccsf.edu>
2017-02-10 19:54:21 -0800

    fix deploy

--- main.rb
+++ main.rb
@@ -9,6 +9,9 @@ configure :development do
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 end
 
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
 
 
 configure do
