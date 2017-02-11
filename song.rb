99458ee98f64fefed09a81486a36dbe57d69a7e5
eric <hyongzhe@mails.ccsf.edu>
2017-02-11 01:26:49 -0800

    ajax add

--- song.rb
+++ song.rb
@@ -8,6 +8,7 @@ class Song
   property :lyrics, Text
   property :length, Integer
   property :released_on, Date
+  property :likes,Integer,:default => 0
 
   def released_on=(date)
     super Date.strptime(date, '%m/%d/%Y')
@@ -26,6 +27,7 @@ module SongHelpers
   def create_song
     @song = Song.create(params[:song])
   end
+
 end
 
 helpers SongHelpers
@@ -72,4 +74,12 @@ delete '/songs/:id' do
     flash[:notice] = 'Song deleted'
   end
   redirect to('/songs')
+end
+
+post '/songs/:id/like' do
+  @song = find_song
+  @song.likes = @song.likes.next
+  @song.save
+  redirect to"/songs/#{@song.id}" unless request.xhr?
+  slim :like, :layout => false
 end
